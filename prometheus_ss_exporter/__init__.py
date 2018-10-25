ad4a77d4581c6df30919a3b11f054b2ef9516fd6
matthias <matthias.tafelmeier@gmx.net>
2018-10-25 14:14:11 +0200

    exporter: realize configuration
    For varying performance requirements depending on the environement
    employed within, logic shall be able to be toggled onuseful granularity.
    In addition, additive selection of rerelvant flows should reduce the
    noise in the collected data, which can be deluge on highly dynamic traffic
    sustaining nodes.

--- prometheus_ss_exporter/__init__.py
+++ prometheus_ss_exporter/__init__.py
@@ -31,11 +31,12 @@ import sys
 import imp
 import time
 import os
-import re
 import io
 import collections
 import argparse
 import bisect
+import yaml
+import ipaddress as ip_a
 
 
 class Utils:
@@ -89,18 +90,21 @@ class bucket_keep(object):
 
 class ss2_collector(object):
 
-    def __init__(self, args):
+    def __init__(self, args, cnfg):
         ss2_script = Utils.which('ss2')
         self.ss2 = imp.load_source('ss2', ss2_script)
-        self._dflt_bounds = [.10,
-                             .50,
-                             1.00,
-                             5.00,
-                             10.00,
-                             50.00,
-                             100.00,
-                             200.00,
-                             500.00]
+
+        self._form_flow_key = getattr(self, "_form_%s_metric_key" %
+                                            cnfg['logic']
+                                            ['flow_label']
+                                            ['origin_folding'])
+        if cnfg['selection']:
+            self.selector = self.is_selected
+        else:
+            # noop
+            self.selector = lambda flow: True
+        self.cnfg = cnfg
+
         self._reset_io()
 
     def _reset_io(self):
@@ -119,6 +123,7 @@ class ss2_collector(object):
                                                'process'])
         args.tcp = True
         args.process = True
+        args.resolve = True
         args.listen = False
         args.all = False
         args.unix = False
@@ -136,47 +141,113 @@ class ss2_collector(object):
 
         return json.loads(sk_stats_raw)
 
-    def _form_metric_key(self, flow):
-        key = "%s|%s|%s|%s" % (flow['src'],
-                               flow['src_port'],
-                               flow['dst'],
-                               flow['dst_port'])
+    def _form_raw_endpoint_metric_key(self, flow):
+        key = "(SRC#%s|%s)(DST#%s|%s)" % (flow['src'],
+                                          flow['src_port'],
+                                          flow['dst'],
+                                          flow['dst_port'])
         return key
+
+    def _form_pid_condensed_metric_key(self, flow):
+        pids = []
+        for usr, pid_ctxt in flow['usr_ctxt'].items():
+            pids.extend(pid_ctxt.keys())
+        key = "(%s)(DST#%s|%s)" % (",".join(pids),
+                                   flow['dst'],
+                                   flow['dst_port'])
+        return key
+
+    def _stage_metrics(self):
+        self.hists = {}
+        hgram_cnfg = (self.cnfg['logic']
+                               ['histograms'])
+        if (hgram_cnfg['active']):
+            if (hgram_cnfg['latency']
+                          ['active']):
+                self.hists['rtt'] = {
+                            'family': HistogramMetricFamily('tcp_rtt_hist',
+                                                            'tcp flows'
+                                                            'latency outline',
+                                                            unit='ms'),
+                            'buckets': bucket_keep(self.cnfg['logic']
+                                                            ['histograms']
+                                                            ['latency']
+                                                            ['bucket_bounds'])
+                            }
+        self.gauges = {}
+        g_cnfg = (self.cnfg['logic']
+                           ['gauges'])
+        if (g_cnfg['active']):
+            if g_cnfg['rtt']['active']:
+                self.gauges['rtt'] = GaugeMetricFamily('tcp_rtt',
+                                                       'tcp socket stats per'
+                                                       'flow latency[rtt]',
+                                                       labels=['flow'])
+            if g_cnfg['cwnd']['active']:
+                self.gauges['snd_cwnd'] = GaugeMetricFamily('tcp_cwnd',
+                                                            'tcp socket per'
+                                                            'flow congestion'
+                                                            'window stats',
+                                                            labels=['flow'])
+
+    def is_selected(self, flow):
+
+        targeted = self.cnfg['selection']
+        for p_range in targeted['portranges']:
+            if ((flow['dst_port'] < p_range['lower']) or
+               (flow['dst_port'] > p_range['lower'])):
+                return False
+
+        dst_ip = ip_a.ip_address(flow['dst'])
+        for node in targeted['nodes']['ips']:
+            try:
+                if ip_a.ipaddress(node) == dst_ip:
+                    return True
+            except TypeError:
+                # alludes version discrepancy
+                pass
+
+        # deliberatly 'simplified' comparison
+        if flow['dst_host'] not in targeted['nodes']['hosts']:
+            return False
+
+        flow_pids = []
+        flow_cmds = []
+        for usr, pid_ctxt in flow['usr_ctxt'].items():
+            flow_pids.extend(pid_ctxt.keys())
+            flow_cmds.append(pid_ctxt['full_cmd'])
+
+        for pid in targeted['pids']:
+            if pid not in flow_pids:
+                return False
+
+        for cmd in targeted['cmds']:
+            if cmd not in flow_cmds:
+                return False
+
+        return True
+
+    def consume_sample(self, flow):
+        key = self._form_flow_key(flow)
+        for g_k, gauge in self.gauges.items():
+            gauge.add_metric([key], flow['tcp_info'][g_k])
+
+        for h_k, histo in self.hists.items():
+            histo['buckets'].enter(flow['tcp_info'][h_k])
 
     def collect(self):
         stats = self._gather_tcp_stats()
 
-        gauges = {
-                'rtt': GaugeMetricFamily('tcp_rtt',
-                                         'tcp socket stats per'
-                                         'flow latency[rtt]',
-                                         labels=['flow']),
-                'snd_cwnd':  GaugeMetricFamily('tcp_cwnd',
-                                               'tcp socket per'
-                                               'flow congestion window stats',
-                                               labels=['flow'])
-                }
-        hists = {
-                'rtt': {
-                        'family': HistogramMetricFamily('tcp_rtt_hist',
-                                                        'tcp flows'
-                                                        'latency outline',
-                                                        unit='ms'),
-                        'buckets': bucket_keep(self._dflt_bounds)
-                        }
-                }
+        self._stage_metrics()
+
         for flow in stats['TCP']['flows']:
-            key = self._form_metric_key(flow)
-            for g_k, gauge in gauges.items():
-                gauge.add_metric([key], flow['tcp_info'][g_k])
+            if self.selector(flow):
+                self.consume_sample(flow)
 
-            for h_k, histo in hists.items():
-                histo['buckets'].enter(flow['tcp_info'][h_k])
-
-        for gauge in gauges.values():
+        for gauge in self.gauges.values():
             yield gauge
 
-        for h_k, histo in hists.items():
+        for h_k, histo in self.hists.items():
             buckets, _sum = histo['buckets'].reveal()
             histo['family'].add_metric([], buckets, _sum)
             yield histo['family']
@@ -194,15 +265,34 @@ def setup_args():
         help='Listen to this port',
         default=8020
     )
+    parser.add_argument(
+        '-c', '--cnfg',
+        metavar='CNFG',
+        required=False,
+        type=str,
+        help='Exporter config file',
+        default=os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                             "../cnfg.yml")
+    )
 
     return parser.parse_args()
+
+
+def setup_cnfg(_file):
+
+    with open(_file, "r") as cnfg_f:
+        cnfg = yaml.load(cnfg_f)
+
+    return cnfg['cnfg']
 
 
 def main():
     try:
         args = setup_args()
+        cnfg = setup_cnfg(args.cnfg)
+        print cnfg
         port = int(args.port)
-        REGISTRY.register(ss2_collector(args))
+        REGISTRY.register(ss2_collector(args, cnfg))
         start_http_server(port)
         print("Serving at port: %s" % port)
         while True:
