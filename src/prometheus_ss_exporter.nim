14180bf01bf5549a3cbd305902ff955dcc78e719
Matthias Tafelmeier <matthias.tafelmeier@gmx.net>
2025-11-27 08:54:37 +0100

    assure license header for all new logic

--- src/prometheus_ss_exporter.nim
+++ src/prometheus_ss_exporter.nim
@@ -2,7 +2,7 @@
 #[
 MIT License
 prometheus_ss_exporter - Prometheus socket statistics exporter in Nim
-Copyright (c) 2024 Rewrite by Matthias Tafelmeier
+Copyright (c) 2018 Matthias Tafelmeier
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
